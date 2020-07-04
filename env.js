c85a10ec9b9a553a2fb08107e7e3d889b0f3acf1
邓鹏 <zootopiagg@gmail.com>
2020-07-04 21:16:16 +0800

    update

--- env.js
+++ env.js
@@ -2,16 +2,16 @@
  * @Author: dengpeng
  * @Date: 2019-08-23 22:59:01
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2019-12-23 16:25:42
+ * @Last Modified time: 2020-07-04 19:55:38
  */
 
 module.exports = {
   dev: {
-    MODE: 'development',
-    ENV_API: 'http://api.55lover.com/api/' //测试服务器地址
+    MODE: "development",
+    ENV_API: "http://localhost:8088/api/" //测试服务器地址
   },
   pro: {
-    MODE: 'production',
-    ENV_API: 'http://api.55lover.com/api/' // 正式服务器地址
+    MODE: "production",
+    ENV_API: "http://api.55lover.com/api/" // 正式服务器地址
   }
-}
+};
